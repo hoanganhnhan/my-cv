87ca69896888b4854f8a26cfcf956da2d8ef5fbb
hoanganhnhan <nhananhhg@gmail.com>
2022-08-09 18:03:08 +0700

    skills and project page

--- src/pages/Projects/index.js
+++ src/pages/Projects/index.js
@@ -1,12 +1,62 @@
+import './Project.scss';
+
 function Projects() {
     return (
-        <>
-            <h2>My projects</h2>
-            <ul>
-                <li>project 1</li>
-                <li>project 2</li>
-            </ul>
-        </>
+        <div className="projects__body">
+            <div className="projects__body__header">
+                <h1>My Projects </h1>
+            </div>
+            <div className="projects__content">
+                <div className="projects__content__item">
+                    <h3 className="projects__content__item__title">Web Dark Film</h3>
+                    <div className="projects__content__item__content">
+                        <li>- Number of member: 4</li>
+                        <li>- My position: FE Developer</li>
+                        <li>- Technical in use: </li>
+                        <li>+ Frontend: HTML, CSS, JavaScript, Bootstrap 5 </li>
+                        <li>+ Backend: NodeJS, ExpressJS</li>
+                        <li>+ Database: NoSQL (MongoDB)</li>
+                        <li className="projects__content__item__has__link">
+                            <a
+                                className="projects__content__item__link"
+                                href="https://github.com/hoanganhnhan/web-film"
+                            >
+                                Click here
+                            </a>
+                        </li>
+                    </div>
+                </div>
+                <div className="projects__content__item">
+                    <h3 className="projects__content__item__title">Web The Band</h3>
+                    <div className="projects__content__item__content">
+                        <li>- Personal project</li>
+                        <li>- My position: FE Developer</li>
+                        <li>- Technical in use: HTML, CSS, JavaScript</li>
+                        <li className="projects__content__item__has__link">
+                            <a className="projects__content__item__link" href="https://the-band-web.herokuapp.com/">
+                                Click here
+                            </a>
+                        </li>
+                    </div>
+                </div>
+                <div className="projects__content__item">
+                    <h3 className="projects__content__item__title">My CV</h3>
+                    <div className="projects__content__item__content">
+                        <li>- Personal project</li>
+                        <li>- My position: FE Developer</li>
+                        <li>- Technical in use: HTML, CSS, JavaScript, ReactJS</li>
+                        <li className="projects__content__item__has__link">
+                            <a
+                                className="projects__content__item__link"
+                                href="https://github.com/hoanganhnhan/web-film"
+                            >
+                                Click here
+                            </a>
+                        </li>
+                    </div>
+                </div>
+            </div>
+        </div>
     );
 }
 
--- src/pages/Skills/index.js
+++ src/pages/Skills/index.js
@@ -7,20 +7,56 @@ function Skills() {
                 <h1>Skills </h1>
             </div>
             <div className="skills__content">
-                <div className="skills__content__technical">
-                    <h2>Technical Skills</h2>
-                    <ul>
-                        <li>HTML, CSS</li>
-                        <li>JavaScript</li>
-                        <li>ReactJS</li>
-                    </ul>
+                <div className="skills__content__list">
+                    <h3>Technical Skills</h3>
+                    <div className="skills__content__list__items">
+                        <ul className="skills__content__list__items__main">
+                            <span>Main</span>
+                            <li className="skills__content__item__list">
+                                <i className="icon__check fa-solid fa-circle-check"></i> HTML, CSS, JavaScript
+                            </li>
+                            <li className="skills__content__item__list">
+                                <i className="icon__check fa-solid fa-circle-check"></i> ReactJS
+                            </li>
+                            <li className="skills__content__item__list">
+                                <i className="icon__check fa-solid fa-circle-check"></i> GIT
+                            </li>
+                            <li className="skills__content__item__list">
+                                <i className="icon__check fa-solid fa-circle-check"></i> Linux (Ubuntu, Kali, CentOS 7)
+                            </li>
+                            <li className="skills__content__item__list">
+                                <i className="icon__check fa-solid fa-circle-check"></i> Windows
+                            </li>
+                        </ul>
+                        <ul className="skills__content__list__items__main">
+                            <span>Other</span>
+                            <li className="skills__content__item__list">
+                                <i className="icon__check fa-solid fa-circle-check"></i> Python, Java, C/C++
+                            </li>
+                            <li className="skills__content__item__list">
+                                <i className="icon__check fa-solid fa-circle-check"></i> SQL Server, NoSQL, JSON, API
+                            </li>
+                            <li className="skills__content__item__list">
+                                <i className="icon__check fa-solid fa-circle-check"></i> Server (Ngrok, Nginx, Apache)
+                            </li>
+                        </ul>
+                    </div>
                 </div>
-                <div className="skills__content__soft">
-                    <h2>Soft skills</h2>
-                    <ul>
-                        <li>HTML, CSS</li>
-                        <li>JavaScript</li>
-                        <li>ReactJS</li>
+                <div className="skills__content__list">
+                    <h3>Soft skills</h3>
+                    <ul className="skills__content__list__items soft__skills">
+                        <li className="skills__content__item__list">
+                            <i className="icon__check fa-solid fa-circle-check"></i> Time management
+                        </li>
+                        <li className="skills__content__item__list">
+                            <i className="icon__check fa-solid fa-circle-check"></i> Attention to details
+                        </li>
+                        <li className="skills__content__item__list">
+                            <i className="icon__check fa-solid fa-circle-check"></i> Problem solving
+                        </li>
+                        <li className="skills__content__item__list">
+                            <i className="icon__check fa-solid fa-circle-check"></i> Teamwork
+                        </li>
                     </ul>
                 </div>
             </div>
